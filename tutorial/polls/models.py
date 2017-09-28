9d7c89742e657369dfb2d886404f3989b0c30ab3
Kyeong Lee <kaythechemist@gmail.com>
2017-09-28 16:26:50 +0900

    Question, Choice __str__  등록

--- tutorial/polls/models.py
+++ tutorial/polls/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 
+
 class Question(models.Model):
     title = models.CharField(max_length=50)
-    published_date = models.DateTimeField()
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
-        return print('{}'.format(self.title))
+        return f'설문조사 ({self.title})'
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     title = models.CharField(max_length=50)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.title} (설문: {self.question.title})'
