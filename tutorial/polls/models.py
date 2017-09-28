781380b394450ed6eb59c058cd65cf746b83b5af
Kyeong Lee <kaythechemist@gmail.com>
2017-09-28 16:21:29 +0900

    Question, Choice 모델 생성 admin 등록

--- tutorial/polls/models.py
+++ tutorial/polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=50)
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return print('{}'.format(self.title))
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    title = models.CharField(max_length=50)
+    votes = models.IntegerField(default=0)
